2428fbd6a8f22aa92e2ad54ac06133c162601217
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-18 03:26:51 +0100

    Complete Stage 1/7: 2+2

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,4 +1,47 @@
 # Author: Zaid Neurothrone
 
-numbers = [int(x) for x in input().split(" ")]
-print(sum(numbers))
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def divide(a, b):
+        if not b:
+            return 0
+        return a / b
+
+    @staticmethod
+    def divide_integer(a, b):
+        if not b:
+            return 0
+        return a // b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def modulus(a, b):
+        return a % b
+
+    @staticmethod
+    def power(a, b):
+        return a ** b
+
+
+def run():
+    a, b = [int(x) for x in input().split(" ")]
+    print(Calculator.add(a, b))
+
+
+if __name__ == "__main__":
+    run()
